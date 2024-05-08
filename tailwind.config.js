2dd93e2a47d5d583629129fd25ed48bb1126019d
pritam guha <pritam@tenxerlabs.com>
2024-05-08 19:39:43 +0530

    added icons and custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.25rem"
+      },
+      colors: {
+        primary: "#00D991"
+      }
+    },
   },
   plugins: [],
 }
